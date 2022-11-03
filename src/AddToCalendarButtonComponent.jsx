2446491e9f8fee6ad60088be712eaf0802cc3b0a
turcuciprian <turcuciprian1@gmail.com>
2022-11-03 21:03:30 +0200

    initial proposal

--- src/AddToCalendarButtonComponent.jsx
+++ src/AddToCalendarButtonComponent.jsx
@@ -1,1 +1,45 @@
-import React from 'react';
+import { atcb_init } from "add-to-calendar-button"
+import 'add-to-calendar-button/assets/css/atcb.css';
+import { useEffect, useMemo } from "react";
+
+export const AddToCalendarButton = ({ name = "Add the title of your event",
+    description = "A nice description does not hurt",
+    startDate = "2022-02-21",
+    endDate = "2022-03-24",
+    startTime = "10:13",
+    endTime = "17:57",
+    location = "Somewhere over the rainbow",
+    label = "Add to Calendar",
+    options = [
+        "Apple",
+        "Google",
+        "iCal",
+        "Microsoft365",
+        "MicrosoftTeams",
+        "Outlook.com",
+        "Yahoo"
+    ],
+    timeZone = "Europe/Berlin",
+    iCalFileName = "Reminder-Event" }) => {
+    useEffect(() => {
+        return atcb_init
+    }, [])
+    const config = useMemo(() => `
+    {
+        "name":"${name}",
+        "description":"${description}",
+        "startDate":"${startDate}",
+        "endDate":"${endDate}",
+        "startTime":"${startTime}",
+        "endTime":"${endTime}",
+        "location":"${location}",
+        "label":"${label}",
+        "options":[${options.map(item => `"${item}"`)}],
+        "timeZone":"${timeZone}",
+        "iCalFileName":"${iCalFileName}"
+        }
+    `, [])
+    return <div className="atcb" style={{ display: 'none' }}>
+        {config.toString()}
+    </div>
+}
