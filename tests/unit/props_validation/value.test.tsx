519c77ee1cd0d99b869684afec66208df3ada15a
Jens Kuerschner <mail@jenskuerschner.de>
2023-12-28 15:10:48 +0100

    exporting atcb_action

--- tests/unit/props_validation/value.test.tsx
+++ tests/unit/props_validation/value.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { expect, test, describe, vi } from 'vitest';
-import { AddToCalendarButton } from '../../../src/index';
+import { AddToCalendarButton, AddToCalendarButtonType } from '../../../src/index';
 import { render } from '@testing-library/react';
 import { defaultProps, invalidOptionsPropValue } from '../../mocks';
 
@@ -8,7 +8,14 @@ describe('props value validation', () => {
   test('throws error with invalid `option` prop value', () => {
     const spy = vi.spyOn(global.console, 'error');
 
-    render(<AddToCalendarButton name={defaultProps.name} debug startDate={defaultProps.startDate} options={invalidOptionsPropValue} />);
+    const config = {
+      name: defaultProps.name,
+      startDate: defaultProps.startDate,
+      options: invalidOptionsPropValue,
+      debug: true
+    } as AddToCalendarButtonType;
+
+    render(<AddToCalendarButton {...config} />);
 
     expect(spy).toHaveBeenCalledTimes(1);
     expect(Array.isArray(spy.mock.lastCall) ? spy.mock.lastCall.join() : '').toContain(`invalid option [${invalidOptionsPropValue.join().toLowerCase()}]`);
@@ -17,7 +24,7 @@ describe('props value validation', () => {
   test('throws error with endDate before startDate', () => {
     const spy = vi.spyOn(global.console, 'error');
 
-    render(<AddToCalendarButton {...defaultProps} debug startTime="14:00" endDate="2055-02-25" endTime="13:00" />);
+    render(<AddToCalendarButton {...defaultProps as AddToCalendarButtonType} debug startTime="14:00" endDate="2055-02-25" endTime="13:00" />);
 
     expect(spy).toHaveBeenCalledTimes(1);
     expect(Array.isArray(spy.mock.lastCall) ? spy.mock.lastCall.join() : '').toContain('end date before start date');
@@ -26,7 +33,7 @@ describe('props value validation', () => {
   test('throws error with invalid timezone', () => {
     const spy = vi.spyOn(global.console, 'error');
 
-    render(<AddToCalendarButton {...defaultProps} debug startTime="14:00" endDate="2055-02-25" endTime="15:00" timeZone="Mars/FantasyCity" />);
+    render(<AddToCalendarButton {...defaultProps as AddToCalendarButtonType} debug startTime="14:00" endDate="2055-02-25" endTime="15:00" timeZone="Mars/FantasyCity" />);
 
     expect(spy).toHaveBeenCalledTimes(2); // 2 times, because the timeZones lib will also throw an error here.
     expect(Array.isArray(spy.mock.lastCall) ? spy.mock.lastCall.join() : '').toContain('invalid time zone given');
